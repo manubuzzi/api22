169851de479a3edadaaf948dc8df5935c7b363e1
manubuzzi <manubuzzi13@gmail.com>
2021-06-27 00:10:06 -0300

    estudiando

---
+++ src/app/Modelo/Persona.ts
@@ -0,0 +1,5 @@
+export class Persona{
+    Id!: number;
+    name!: string;
+    apellidos!: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './persona/listar/listar.component';
+import { AddComponent } from './persona/add/add.component';
+import { EditComponent } from './persona/edit/edit.component';
+import {FormsModule} from '@angular/forms';
+import {ServiceService} from '../app/service/service.service'
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
